c2279c75fdbfd2cec8556b02d6cac2492f6286c0
Daniel Spitzley <daniels@oink.de>
2022-03-26 11:20:00 +0100

    Added missing ArtifactsQueryOptions

---
+++ src/GitLabApiClient/Models/Artifacts_/Requests/ArtifactsQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLabApiClient.Models.Artifacts.Requests
+{
+    public class ArtifactsQueryOptions
+    {
+        public string JobToken { get; set; }
+    }
+}
